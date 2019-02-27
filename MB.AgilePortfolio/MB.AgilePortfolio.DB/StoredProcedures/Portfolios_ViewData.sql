eef6f3a961fd43dd68bf854d5b5445c4a5b402fa
Brody Lemke <500161066@fvtc.edu>
2019-02-27 17:54:39 -0600

    Stored Procedure functionality to recieve a single portfolio

--- MB.AgilePortfolio/MB.AgilePortfolio.DB/StoredProcedures/Portfolios_ViewData.sql
+++ MB.AgilePortfolio/MB.AgilePortfolio.DB/StoredProcedures/Portfolios_ViewData.sql
@@ -1,8 +1,17 @@
 ﻿CREATE PROCEDURE [dbo].[Portfolio_ViewData]
-
+	@ProjectId UNIQUEIDENTIFIER = null
 AS
-	SELECT p.Id, p.[Name], p.[Description], p.PortfolioImage, u.Email, u.FirstName, u.LastName 
-	FROM [dbo].tblPortfolio p
-	JOIN [dbo].tblUser u
-	ON u.Id = p.UserId;
+	IF (@ProjectId = null)
+		SELECT p.Id, p.[Name], p.[Description], p.PortfolioImage, u.Email, u.FirstName, u.LastName 
+		FROM [dbo].tblPortfolio p
+		JOIN [dbo].tblUser u
+		ON u.Id = p.UserId;
+
+	--Optional functionality to retrieve a specific portfolio's information when supplied with an Id
+	ELSE 
+		SELECT p.Id, p.[Name], p.[Description], p.PortfolioImage, u.Email, u.FirstName, u.LastName 
+		FROM [dbo].tblPortfolio p
+		JOIN [dbo].tblUser u
+		ON u.Id = p.UserId
+		WHERE p.Id = @ProjectId;
 RETURN 0
