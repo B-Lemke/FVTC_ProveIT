319c13402ce22bcc1d8b6ab588f2be51e7bac8d8
Brody Lemke <500161066@fvtc.edu>
2019-03-08 10:22:47 -0600

    Setting up Font-Awesome, Admin Homepage, Editing Titles on Pages

---
+++ MB.AgilePortfolio/MB.AgilePortfolio.MVCUI/Controllers/AdminController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MB.AgilePortfolio.BL;
+using MB.AgilePortfolio.MVCUI.ViewModels;
+
+namespace MB.AgilePortfolio.MVCUI.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: Admin
+        public ActionResult Admin()
+        {
+            return View();
+        }
+
+    }
+}
