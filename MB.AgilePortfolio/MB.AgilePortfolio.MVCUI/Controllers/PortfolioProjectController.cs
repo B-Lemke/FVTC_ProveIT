6120b068f890c91e68acac6e5e827c168b670c43
Brody Lemke <500161066@fvtc.edu>
2019-03-09 16:28:23 -0600

    Portfolio Project CRUD and Controller and View added.

---
+++ MB.AgilePortfolio/MB.AgilePortfolio.MVCUI/Controllers/PortfolioProjectController.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MB.AgilePortfolio.BL;
+using MB.AgilePortfolio.MVCUI.ViewModels;
+
+namespace MB.AgilePortfolio.MVCUI.Controllers
+{
+    public class PortfolioProjectController : Controller
+    {
+        PortfolioProjectList portfolioProjects;
+
+        // GET: PortfolioProject
+        public ActionResult Index()
+        {
+            portfolioProjects = new PortfolioProjectList();
+            portfolioProjects.Load();
+            return View(portfolioProjects);
+        }
+
+        // GET: PortfolioProject/Details/5
+        public ActionResult Details(Guid id)
+        {
+            PortfolioProjectViewModel ppvm = new PortfolioProjectViewModel()
+            {
+                PortfolioProject = new PortfolioProject(),
+                Portfolios = new PortfolioList(),
+                Projects = new ProjectList(),
+            };
+            ppvm.PortfolioProject.LoadById(id);
+            ppvm.Projects.Load();
+            ppvm.Portfolios.Load();
+
+            //Set display names
+            Portfolio port = ppvm.Portfolios.FirstOrDefault(p => p.Id == ppvm.PortfolioProject.PortfolioId);
+            ppvm.portfolioName = (port != null) ? port.Name : ppvm.PortfolioProject.PortfolioId.ToString();
+            Project proj = ppvm.Projects.FirstOrDefault(p => p.Id == ppvm.PortfolioProject.ProjectId);
+            ppvm.portfolioName = (proj != null) ? proj.Name : ppvm.PortfolioProject.ProjectId.ToString();
+
+
+            return View(ppvm);
+        }
+
+        // GET: PortfolioProject/Create
+        public ActionResult Create()
+        {
+            PortfolioProjectViewModel ppvm = new PortfolioProjectViewModel()
+            {
+                PortfolioProject = new PortfolioProject(),
+                Portfolios = new PortfolioList(),
+                Projects = new ProjectList(),
+            };
+            ppvm.Projects.Load();
+            ppvm.Portfolios.Load();
+            
+            return View(ppvm);
+        }
+
+        // POST: PortfolioProject/Create
+        [HttpPost]
+        public ActionResult Create(PortfolioProjectViewModel ppvm )
+        {
+            try
+            {
+                // TODO: Add insert logic here
+                ppvm.PortfolioProject.Insert();
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: PortfolioProject/Edit/5
+        public ActionResult Edit(Guid id)
+        {
+            PortfolioProjectViewModel ppvm = new PortfolioProjectViewModel()
+            {
+                PortfolioProject = new PortfolioProject(),
+                Portfolios = new PortfolioList(),
+                Projects = new ProjectList(),
+            };
+            ppvm.PortfolioProject.LoadById(id);
+            ppvm.Projects.Load();
+            ppvm.Portfolios.Load();
+
+            Portfolio port = ppvm.Portfolios.FirstOrDefault(p => p.Id == ppvm.PortfolioProject.PortfolioId);
+            ppvm.portfolioName = (port != null) ? port.Name : ppvm.PortfolioProject.PortfolioId.ToString();
+
+            Project proj = ppvm.Projects.FirstOrDefault(p => p.Id == ppvm.PortfolioProject.ProjectId);
+            ppvm.portfolioName = (proj != null) ? proj.Name : ppvm.PortfolioProject.ProjectId.ToString();
+
+            return View(ppvm);
+        }
+
+        // POST: PortfolioProject/Edit/5
+        [HttpPost]
+        public ActionResult Edit(Guid id, PortfolioProjectViewModel ppvm)
+        {
+            try
+            {
+                // TODO: Add update logic here
+                ppvm.PortfolioProject.Update();
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View(ppvm);
+            }
+        }
+
+        // GET: PortfolioProject/Delete/5
+        public ActionResult Delete(Guid id)
+        {
+            PortfolioProjectViewModel ppvm = new PortfolioProjectViewModel()
+            {
+                PortfolioProject = new PortfolioProject(),
+                Portfolios = new PortfolioList(),
+                Projects = new ProjectList(),
+            };
+            ppvm.PortfolioProject.LoadById(id);
+            ppvm.Projects.Load();
+            ppvm.Portfolios.Load();
+
+            //Set display names
+            Portfolio port = ppvm.Portfolios.FirstOrDefault(p => p.Id == ppvm.PortfolioProject.PortfolioId);
+            ppvm.portfolioName = (port != null) ? port.Name : ppvm.PortfolioProject.PortfolioId.ToString();
+            Project proj = ppvm.Projects.FirstOrDefault(p => p.Id == ppvm.PortfolioProject.ProjectId);
+            ppvm.portfolioName = (proj != null) ? proj.Name : ppvm.PortfolioProject.ProjectId.ToString();
+
+            return View(ppvm);
+        }
+
+        // POST: PortfolioProject/Delete/5
+        [HttpPost]
+        public ActionResult Delete(Guid id, PortfolioProject pp)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+                pp.Delete();
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View(pp);
+            }
+        }
+    }
+}
---
+++ MB.AgilePortfolio/MB.AgilePortfolio.MVCUI/ViewModels/PortfolioProjectViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Web;
+using MB.AgilePortfolio.BL;
+
+namespace MB.AgilePortfolio.MVCUI.ViewModels
+{
+    public class PortfolioProjectViewModel
+    {
+        public PortfolioProject PortfolioProject { get; set; }
+        public PortfolioList Portfolios { get; set; }
+        public ProjectList Projects { get; set; }
+
+        [DisplayName("Portfolio Name")]
+        public string portfolioName { get; set; }
+
+        [DisplayName("Project Name")]
+        public string projectName { get; set; }
+    }
+}
