7b19aeef6e5125fa3b91477923d685e3c4d79ae2
Brenton Wienkes <actofmagic8@hotmail.com>
2019-03-10 14:47:53 -0500

    adding New Account creation

---
+++ MB.AgilePortfolio/MB.AgilePortfolio.MVCUI/Controllers/NewAccountController.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MB.AgilePortfolio.BL;
+using MB.AgilePortfolio.MVCUI.ViewModels;
+
+namespace MB.AgilePortfolio.MVCUI.Controllers
+{
+    public class NewAccountController : Controller
+    {
+        User user;
+        UserList users;
+        
+        // GET: User
+        public ActionResult Index()
+        {
+            users = new UserList();
+            users.Load();
+            return View(users);
+        }
+
+        // GET: User/Details/5
+        public ActionResult Details(Guid id)
+        {
+            user = new User();
+            user.LoadById(id);
+            return View(user);
+        }
+
+        // GET: User/Create
+        public ActionResult Create()
+        {
+            UserUserTypes uut = new UserUserTypes()
+            {
+                User = new User(),
+                UserTypes = new UserTypeList()
+            };
+            uut.UserTypes.Load();
+            return View(uut);
+        }
+
+        // POST: User/Create
+        [HttpPost]
+        public ActionResult Create(UserUserTypes uut)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+                //uut.User.Email
+                //users = new UserList();
+                //if(uut.User.Email == null || users.e)
+
+                if(uut.User.Email == null)
+                {
+                    ModelState.AddModelError(string.Empty, "Email address is required");
+                    return RedirectToAction("Create", uut);
+                }
+
+                uut.User.Insert();
+                return RedirectToAction("Create");
+            }
+            catch { return View(uut); }
+        }
+
+       
+    }
+}
