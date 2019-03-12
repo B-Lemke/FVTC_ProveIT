a270b01b3978e9093f8b97f1929082a1151df450
Brody Lemke <500161066@fvtc.edu>
2019-03-11 22:43:56 -0500

    Applying bootstrap theme to the website and adding in the scripts and updating the navbar.

--- MB.AgilePortfolio/MB.AgilePortfolio.MVCUI/App_Start/BundleConfig.cs
+++ MB.AgilePortfolio/MB.AgilePortfolio.MVCUI/App_Start/BundleConfig.cs
@@ -20,11 +20,25 @@ namespace MB.AgilePortfolio.MVCUI
                         "~/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js"));
+                      "~/Scripts/bootstrap.js",
+                      "~/Scripts/contact_me.js",
+                      "~/Scripts/jqBootstrapValidation.js",
+                      "~/Scripts/jquery.magnific-popup.js",
+                      "~/Scripts/freelancer.js"
+
+                      ));
+
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/site.css",
+                      "~/Content/font-awesome.css",
+                      "~/Content/freelancer.css"
+                      ));
+
+
+
+
         }
     }
 }
