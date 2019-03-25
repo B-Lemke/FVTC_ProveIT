5bd8c634f4b41da5ecafd84297386cad8b6c0cf6
Brody Lemke <500161066@fvtc.edu>
2019-03-24 22:44:02 -0500

    Added a homepage and about page and updated the links in the footer. Trying to clean things up for our deploy.

--- MB.AgilePortfolio/MB.AgilePortfolio.MVCUI/App_Start/RouteConfig.cs
+++ MB.AgilePortfolio/MB.AgilePortfolio.MVCUI/App_Start/RouteConfig.cs
@@ -42,9 +42,17 @@ namespace MB.AgilePortfolio.MVCUI
             );
 
             routes.MapRoute(
+                name: "About",
+                url: "About",
+                defaults: new { controller = "Home", action = "About"}
+            );
+
+
+
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Admin", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
