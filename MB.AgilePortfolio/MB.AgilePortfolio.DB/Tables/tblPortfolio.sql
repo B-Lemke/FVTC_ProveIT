5a4fa1f94a386eee8cb7f3dcf39b4625fc6bb1b5
Brody Lemke <500161066@fvtc.edu>
2019-04-14 17:23:29 -0500

    DB project datatype changes and added bio field to the tblUser. Didn't publish, but updated for publish.

--- MB.AgilePortfolio/MB.AgilePortfolio.DB/Tables/tblPortfolio.sql
+++ MB.AgilePortfolio/MB.AgilePortfolio.DB/Tables/tblPortfolio.sql
@@ -2,7 +2,7 @@
 (
 	[Id] UNIQUEIDENTIFIER NOT NULL PRIMARY KEY, 
     [Name] VARCHAR(255) NOT NULL, 
-    [Description] VARCHAR(255) NULL, 
+    [Description] TEXT NULL, 
     [PortfolioImage] VARCHAR(255) NULL, 
     [UserId] UNIQUEIDENTIFIER NOT NULL, 
     [PrivacyId] UNIQUEIDENTIFIER NOT NULL
